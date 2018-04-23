8898ac263c265d892912de5573c196d2e32942a1
thomas <onixyros@gmail.com>
2018-04-23 22:48:26 +0200

    mise en place bootstrap quasi terminée

--- javascript/js.js
+++ javascript/js.js
@@ -1,52 +1,74 @@
-$('div').hide();
-$('#winner').hide();
-var chiffre_mystere = Math.floor(Math.random()* 11);
-var i = 1;
+//On initialise nos variables, on fais le *10 sur le random pour que le chiffre soit compris en 0 et 9
+var chiffre_mystere = Math.floor(Math.random()* 10);
+var i = 0;
+var musique = document.createElement("audio");
+musique.src="sons/remix_pokemon.mp3";
+musique.volume=0.5;
+musique.autoPlay=false;
+musique.preLoad=true;  
+
+//On cache les divs que l'on ne veut pas voir
+$('#scene_2, #scene_3').toggle();
+
+$('#btn_debut').click(function()
+{
+    $('#scene_1, #scene_2').toggle();
+});
+
+$('#btn_combat').click(function()
+{
+    $('#scene_2, #scene_3, .initialisation').toggle();
+    musique.play();
+});
+
+//Pour afficher le résultat
 $('.resultat').replaceWith("",chiffre_mystere);
-// console.log(chiffre_mystere);
 
 $('#bouton').click(function verif()
 {
+    //On récupère la valeur de l'input
     var value_champ =$('#champ_reponse').val();
-    //On recache les div pour "réinitialiser" les divs affichées.
-    $('div').hide();
+    i++;
+
+    //Indique si l'on gagne ou pas
+    //Cas où chiffre inscrit < chiffre mystère
     if (value_champ<chiffre_mystere)
     {
         $('#plus').show();
-        i++;
+        $('#moins').hide();
     }
+    //Cas où chiffre inscrit > chiffre mystère
     else if (value_champ>chiffre_mystere)
     {
         $('#moins').show();
-        i++;
+        $('#plus').hide();
     }
-    else
+    //Cas où chiffre inscrit = chiffre mystère
+    else if(value_champ=chiffre_mystere)
     {
-        $('#win').show();
-        $('#bouton').hide();
-        $('#winner').show();
-        $('#coeur1').hide();
-        $('#coeur2').hide();
-        $('#coeur3').hide();
+        //Pour avoir le nombre de tentatives
+        $('#tentatives').replaceWith("",i);
+        $('#win, #reload').show();
+        $('#bouton, #coeurs, .try, .img_mamie, #plus, #moins').hide();
     }
 
-    if (i==2)
+    //Indique le nombre de coeurs restant ainsi que le message à afficher en fonction des vies
+    if (i==1 && value_champ != chiffre_mystere)
     {
-        $('#coeur1').hide();
+        $('#coeur1, #3try, #mamie_3, #2try, #mamie_2').toggle();
     }
-    if (i==3)
+    if (i==2 && value_champ != chiffre_mystere)
     {
-        $('#coeur2').hide();
+        $('#coeur2, #2try, #mamie_2, #1try, #mamie_1').toggle();
     }
-    if (i>3)
+    if (i==3 && value_champ != chiffre_mystere)
     {
-        $('#bouton').hide();
-        $('#coeur3').hide();
-        $('#lose').show();
+        $('#bouton, #coeur3, #lose, #reload, #1try, #mamie_1').toggle();
+        $('#plus, #moins').hide();
     }
 });
 
-$('.reload').click(function rechargement()
+$('#reload').click(function rechargement()
 {
     location.reload();
 });
