bd0eae44e43943c5baf244d8d2a83351880f797d
thomas <onixyros@gmail.com>
2018-04-24 10:24:57 +0200

    ajout document ready

--- javascript/js.js
+++ javascript/js.js
@@ -26,48 +26,50 @@ $('#btn_combat').click(function()
 //Pour afficher le résultat
 $('.resultat').replaceWith("",chiffre_mystere);
 
-$('#bouton').click(function verif()
-{
-    //On récupère la valeur de l'input
-    var value_champ =$('#champ_reponse').val();
-    i++;
+$(document).ready(function(){
+    $('#bouton').click(function verif()
+    {
+        //On récupère la valeur de l'input
+        var value_champ =$('#champ_reponse').val();
+        i++;
 
-    //Indique si l'on gagne ou pas
-    //Cas où chiffre inscrit < chiffre mystère
-    if (value_champ<chiffre_mystere)
-    {
-        $('#plus').show();
-        $('#moins').hide();
-    }
-    //Cas où chiffre inscrit > chiffre mystère
-    else if (value_champ>chiffre_mystere)
-    {
-        $('#moins').show();
-        $('#plus').hide();
-    }
-    //Cas où chiffre inscrit = chiffre mystère
-    else if(value_champ=chiffre_mystere)
-    {
-        //Pour avoir le nombre de tentatives
-        $('#tentatives').replaceWith("",i);
-        $('#win, #reload').show();
-        $('#bouton, #coeurs, .try, .img_mamie, #plus, #moins').hide();
-    }
+        //Indique si l'on gagne ou pas
+        //Cas où chiffre inscrit < chiffre mystère
+        if (value_champ<chiffre_mystere)
+        {
+            $('#plus').show();
+            $('#moins').hide();
+        }
+        //Cas où chiffre inscrit > chiffre mystère
+        else if (value_champ>chiffre_mystere)
+        {
+            $('#moins').show();
+            $('#plus').hide();
+        }
+        //Cas où chiffre inscrit = chiffre mystère
+        else if(value_champ=chiffre_mystere)
+        {
+            //Pour avoir le nombre de tentatives
+            $('#tentatives').replaceWith("",i);
+            $('#win, #reload').show();
+            $('#bouton, #coeurs, .try, .img_mamie, #plus, #moins').hide();
+        }
 
-    //Indique le nombre de coeurs restant ainsi que le message à afficher en fonction des vies
-    if (i==1 && value_champ != chiffre_mystere)
-    {
-        $('#coeur1, #3try, #mamie_3, #2try, #mamie_2').toggle();
-    }
-    if (i==2 && value_champ != chiffre_mystere)
-    {
-        $('#coeur2, #2try, #mamie_2, #1try, #mamie_1').toggle();
-    }
-    if (i==3 && value_champ != chiffre_mystere)
-    {
-        $('#bouton, #coeur3, #lose, #reload, #1try, #mamie_1').toggle();
-        $('#plus, #moins').hide();
-    }
+        //Indique le nombre de coeurs restant ainsi que le message à afficher en fonction des vies
+        if (i==1 && value_champ != chiffre_mystere)
+        {
+            $('#coeur1, #3try, #mamie_3, #2try, #mamie_2').toggle();
+        }
+        if (i==2 && value_champ != chiffre_mystere)
+        {
+            $('#coeur2, #2try, #mamie_2, #1try, #mamie_1').toggle();
+        }
+        if (i==3 && value_champ != chiffre_mystere)
+        {
+            $('#bouton, #coeur3, #lose, #reload, #1try, #mamie_1').toggle();
+            $('#plus, #moins').hide();
+        }
+    });
 });
 
 $('#reload').click(function rechargement()
